3694c9b3b4f34bb7f237891762526915fd301875
Aparna218 <aparnadhere123@gmail.com>
2022-10-27 15:13:48 +0530

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Aparna324:olU1kKXqMpuVasg6@clusteraparna.brurb5d.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
